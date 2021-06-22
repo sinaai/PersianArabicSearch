5a395644167a636d51f6c9b19a3fd4a3bc7fc0fc
Sina Bagherinezhad <sinakhan@live.com>
2021-06-22 11:08:03 +0430

    add index of farsi and arabic word to database

--- .virtual_documents/Untitled.ipynb.py
+++
@@ -1,7 +0,0 @@
-from langDetection import language_detector
-
-
-language_detector('ان الانسان')
-
-
-
--- databaseRefactor.py
+++ databaseRefactor.py
@@ -1,18 +1,33 @@
 import hazm
+from nltk.tokenize import WhitespaceTokenizer as wt
 from elasticsearch import Elasticsearch
 from langDetection import language_detector
 
 
-def separate_languages(string, library='voting'):
+def sent_tokenizer(string):
     sentences = hazm.sent_tokenize(string)
+    indices = list(wt().span_tokenize_sents(sentences))
+    idx = 1
+    for s in range(len(indices)):
+        for w in range(len(indices[s])):
+            indices[s][w] = idx
+            idx += 1
+    return sentences, indices
+
+
+def separate_languages(string, library='voting'):
+    sentences, indices = sent_tokenizer(string)
     arabic, farsi = '', ''
-    for sentence in sentences:
+    arabic_idx, farsi_idx = [], []
+    for i, sentence in enumerate(sentences):
         language = language_detector(sentence, library)
         if language == 'fa':
             farsi = farsi + sentence
+            farsi_idx += indices[i]
         else:
             arabic = arabic + sentence
-    return farsi, arabic
+            arabic_idx += indices[i]
+    return farsi, arabic, farsi_idx, arabic_idx
 
 
 def rebuild_index(index_name, text_field, language_detection_library='voting'):
@@ -20,13 +35,12 @@ def rebuild_index(index_name, text_field, language_detection_library='voting'):
     lastID = es.count(index=index_name)['count']
     for i in range(1, lastID + 1):
         txt = es.get(index=index_name, id=i)['_source'][text_field]
-        farsi, arabic = separate_languages(txt, language_detection_library)
+        farsi, arabic, farsi_idx, arabic_idx = separate_languages(txt, language_detection_library)
         doc = {
             text_field: txt,
             "farsi_field": farsi,
-            "arabic_field": arabic
+            "arabic_field": arabic,
+            "farsi_indices": farsi_idx,
+            "arabic_indices": arabic_idx
         }
         es.index(index=index_name, id=i, body=doc)
-
-
-
