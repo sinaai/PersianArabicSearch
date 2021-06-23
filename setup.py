ea31be7251fd696710983eaeb3d917610f048e60
Sina Bagherinezhad <sinakhan@live.com>
2021-06-23 19:46:25 +0430

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Persian-Arabic Search',
+    version='1.1',
+    packages=[''],
+    url='sinaai.github.io',
+    license='',
+    author='sina',
+    author_email='sinakhan@live.com',
+    description=''
+)
